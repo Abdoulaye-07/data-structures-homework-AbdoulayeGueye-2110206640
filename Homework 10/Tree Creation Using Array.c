9f6b66b6b78444c99e60c49ec7baeb9e2e22db60
Abdoulaye-07 <layepena@gmail.com>
2026-01-03 22:42:20 +0300

    Create Tree Creation Using Array.c

---
+++ Homework 10/Tree Creation Using Array.c
@@ -0,0 +1,18 @@
+/*
+Student Name: ABDOULAYE GUEYE
+Student ID: 2110206640
+Homework 10
+Topic: Doubly Linked List
+*/
+
+#include <stdio.h>
+
+int main() {
+    int tree[15] = {10, 5, 20, 3, 7, 15, 25};
+
+    printf("Binary Tree using Array:\n");
+    for(int i = 0; i < 7; i++)
+        printf("Index %d: %d\n", i, tree[i]);
+
+    return 0;
+}
